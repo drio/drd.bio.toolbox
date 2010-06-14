2977213cff5117436cb3f0e3b233a7328cf06f84
David Rio Deiros <driodeiros@gmail.com>
2010-06-14 16:57:25 -0500

    adding dups

--- bam.stats.sh
+++ bam.stats.sh
@@ -10,7 +10,7 @@ usage()
   [ ".$1" != "." ] && echo "ERROR: $1"
   cat <<-EOF
 Usage:
-  `basename $0` <sam/bam file> [n_ends] [min_map_qual]
+  `basename $0` -i <sam/bam file> [-e n_ends] [-m min_map_qual]
   n_ends      : 1 or 2 [1]
   min_map_qual: default [0]
 EOF
@@ -25,11 +25,31 @@ ctrl_c()
 
 trap ctrl_c SIGINT
 
-if=$1
-[ ".$2"  == "." ]  && n_ends=1 || n_ends=$2
-[ ".$3"  == "." ]  && mmq=0 || mmq=$3
+if=""
+n_ends=1
+mmq=0
+while getopts 'i:e:m:h' OPTION
+do
+  case $OPTION in
+    i)
+      if=$OPTARG
+      ;;
+    e)
+      n_ends=$OPTARG
+      ;;
+    m)
+      mmq=$OPTARG
+      ;;
+    ?)
+      usage
+      exit 2
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
 [ ".$if" == "." ] && usage "I need a bam/sam file"
-if [ "$n_ends" != 1 ] && [ "$n_ends" != 2 ] 
+if [ "$n_ends" != 1 ] && [ "$n_ends" != 2 ]
 then
   usage "Number of ends: 1 or 2"
 fi
@@ -50,13 +70,16 @@ fi
 
 if [ $n_ends == 1 ]
 then
-  samtools view $stf -q $mmq $if      | wc -l > ./tr1.$$ &
-  samtools view $stf -q $mmq -F 4 $if | wc -l > ./mr1.$$ &
+  samtools view $stf -q $mmq $if         | wc -l > ./tr1.$$ &
+  samtools view $stf -q $mmq -F 4 $if    | wc -l > ./mr1.$$ &
+  samtools view $stf -q $mmq -f 1024 $if | wc -l > ./d1.$$ &
 else
   samtools view $stf -q $mmq -f 64       $if | wc -l > ./tr1.$$ &
   samtools view $stf -q $mmq -f 128      $if | wc -l > ./tr2.$$ &
   samtools view $stf -q $mmq -f 64 -F 4  $if | wc -l > ./mr1.$$ &
   samtools view $stf -q $mmq -f 128 -F 4 $if | wc -l > ./mr2.$$ &
+  samtools view $stf -q $mmq -f 1088     $if | wc -l > ./d1.$$  &
+  samtools view $stf -q $mmq -f 1152     $if | wc -l > ./d2.$$  &
 fi
 
 sleep 1
@@ -65,22 +88,29 @@ wait
 
 tr1=`cat ./tr1.$$`
 mr1=`cat ./mr1.$$`
+d1=`cat ./d1.$$`
 if [ $n_ends == 2 ]
 then
   tr2=`cat ./tr2.$$`
   mr2=`cat ./mr2.$$`
+  d2=`cat ./d2.$$`
 fi
 
-rm -f tr1.$$ mr1.$$ tr2.$$ mr2.$$
+rm -f tr*.$$ mr*.$$  d*.$$
 
 if [ $n_ends == 1 ]
 then
-  echo -ne "Total  # read1:\t $[$tr1] \n" 
-  echo -ne "mapped # read1:\t $[$mr1] (`echo "scale=2 ; ($mr1*100)/$tr1 " | bc`%) \n"
+  echo -ne "# Total  read1:\t $[$tr1] \n"
+  echo -ne "# mapped read1:\t $[$mr1] (`echo "scale=2 ; ($mr1*100)/$tr1 " | bc`%) \n"
+  echo -ne "# dups   read1:\t $[$d1] (`echo "scale=2 ; ($d1*100)/$tr1 " | bc`%) \n"
 else
-  echo -ne "Total  # reads:\t $[$tr1+$tr2]\n"
-  echo -ne "Total  # read1:\t $[$tr1]\n"
-  echo -ne "Total  # read2:\t $[$tr2]\n"
-  echo -ne "mapped # read1:\t $[$mr1] (`echo "scale=2 ; ($mr1*100)/$tr1 " | bc`%) \n"
-  echo -ne "mapped # read2:\t $[$mr2] (`echo "scale=2 ; ($mr2*100)/$tr2 " | bc`%) \n"
+  echo -ne "# Total  reads:\t $[$tr1+$tr2]\n\n"
+
+  echo -ne "# Total  read1:\t $[$tr1]\n"
+  echo -ne "# mapped read1:\t $[$mr1] (`echo "scale=2 ; ($mr1*100)/$tr1 " | bc`%) \n"
+  echo -ne "# dups   read1:\t $[$d1] (`echo "scale=2 ; ($d1*100)/$tr1 " | bc`%) \n\n"
+
+  echo -ne "# Total  read2:\t $[$tr2]\n"
+  echo -ne "# mapped read2:\t $[$mr2] (`echo "scale=2 ; ($mr2*100)/$tr2 " | bc`%) \n"
+  echo -ne "# dups   read1:\t $[$d2] (`echo "scale=2 ; ($d2*100)/$tr1 " | bc`%) \n"
 fi
