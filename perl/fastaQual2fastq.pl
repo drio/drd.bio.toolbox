a9243e31cbe4e2c16fe187df0d91a4090d9fa19b
David Rio Deiros (drio) <deiros@bcm.edu>
2010-11-24 10:14:21 -0600

    * adding fasta_qual to fastq

---
+++ perl/fastaQual2fastq.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+# 
+# Original code (kmcarr): http://seqanswers.com/forums/showthread.php?t=2775
+# Patched by drio
+#
+use warnings;
+use strict;
+use Data::Dumper;
+use File::Basename;
+
+my $inFasta = $ARGV[0];
+my $baseName = basename($inFasta, qw/.fasta .fna/);
+my $inQual = $baseName . ".qual";
+my $outFastq = $baseName . ".fastq";
+
+my %seqs;
+
+$/ = ">";
+
+open (FASTA, "<$inFasta");
+my $junk = (<FASTA>);
+
+while (my $frecord = <FASTA>) {
+	chomp $frecord;
+	my ($fdef, @seqLines) = split /\n/, $frecord;
+	my $seq = join '', @seqLines;
+	$seqs{$fdef} = $seq;
+}
+
+close FASTA;
+
+open (QUAL, "<$inQual");
+$junk = <QUAL>;
+open (FASTQ, ">$outFastq");
+
+while (my $qrecord = <QUAL>) {
+	chomp $qrecord;
+	my ($qdef, @qualLines) = split /\n/, $qrecord;
+	my $qualString = join ' ', @qualLines;
+	$qualString =~ s/\s+/ /g;
+	my @quals = split / /, $qualString;
+	print FASTQ "@","$qdef\n";
+	print FASTQ "$seqs{$qdef}\n";
+	print FASTQ "+\n";
+	foreach my $qual (@quals) {
+		print FASTQ chr($qual + 33);
+	}
+	print FASTQ "\n";
+}
+
+close QUAL;
+close FASTQ;
