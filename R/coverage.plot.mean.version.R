b62d818f49b8b4314f1afd37674fb757c5570791
David Rio Deiros (drio) <deiros@bcm.edu>
2010-11-30 12:34:24 -0600

    * adding a different way to calculate coverage

---
+++ R/coverage.plot.mean.version.R
@@ -0,0 +1,17 @@
+# cat i.txt | awk '{print $2" "$8 }' | uniq | awk '{print $2}'| calc_coverage_stats 10 > cov.txt
+# i.txt comes from samtools pileup
+#
+args=(commandArgs(TRUE));
+for(i in 1:length(args)){
+  eval(parse(text=args[[i]]));
+}
+
+#pdf(file='./cov.pdf', height=5, width=12)
+#bitmap(file="./cov.png", height=5, width=14)
+#png(file=in.o_file, 8, 3, units="in", res=150)
+bitmap(file=in.o_file, type="png256", width=7, height=2, units="in", res=150)
+#par(mfrow=c(1,1))
+#par(mar=c(11,5,4,2))
+data <- scan(file="stdin", what=list(pos=0, me=0, sd=0), skip=0)
+plot(data$pos, data$me, main=in.title, xlab="window number", 
+ylab="# of reads", col="red", pch=".", cex=.4, ylim=c(0,20), cex.lab=1.2)
--- bash/run_R_coverage_plot.sh
+++ bash/run_R_coverage_plot.sh
@@ -2,23 +2,31 @@
 #
 #set -e 
 
+#script <output.png> <window_size> <title> <pileup_file>
 usage()
 {
   cat<<-EOF
 Usage:
-  script <output.png> <window_size> <title> <pileup_file>
+  script <output.png> <title>
+
+Example:
+$ cat i.txt | awk '{print $2" "$8 }' | uniq |awk '{print $2}'| calc_coverage_stats 50 | run_R_coverage_plot.sh cov.png title_here
+$ samtools pileup my.bam | awk '{print $2" "$8 }' | uniq |awk '{print $2}'| calc_coverage_stats 50 | run_R_coverage_plot.sh cov.png title_here
 EOF
 }
 
 source "`dirname ${BASH_SOURCE[0]}`/common.sh"
 
 d_path=`dirname ${BASH_SOURCE[0]}`
-r_script="${d_path}/../R/coverage.plot.R"
+#r_script="${d_path}/../R/coverage.plot.R"
+r_script="${d_path}/../R/coverage.plot.mean.version.R"
 
-if [ ".$1" == "." ] || [ ".$2" == "." ] || [ ".$3" == "." ] || [ ! -f $4 ]
+#if [ ".$1" == "." ] || [ ".$2" == "." ] || [ ".$3" == "." ] || [ ! -f $4 ]
+if [ ".$1" == "." ] || [ ".$2" == "." ]
 then
   usage
   exit 1
 fi
 
-awk '{print $2" "$8}' $4 | R CMD BATCH "--vanilla --slave --args in.o_file='$1' in.window='$2' in.title='$3'" $r_script
+#awk '{print $2" "$8}' $4 | R CMD BATCH "--vanilla --slave --args in.o_file='$1' in.window='$2' in.title='$3'" $r_script
+R CMD BATCH "--vanilla --slave --args in.o_file='$1' in.title='$2'" $r_script
