9c5bf9410c076d66b4ea4cf7ecad3ecdbdaac889
David Rio Deiros <driodeiros@gmail.com>
2011-05-26 12:07:53 -0500

    * Dist of coverage from bam. Dumps to txt and json.

---
+++ bash/dist_coverage_txt_json.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+set -e
+#set -x
+
+source "`dirname ${BASH_SOURCE[0]}`/common.sh"
+
+usage()
+{
+  [ ".$1" != "." ] && echo "ERROR: $1"
+  echo "Usage:"
+  echo "$0 <bam>"
+  exit 0
+}
+
+if=$1
+
+[ ".$if" == "." ] && usage "I need an input_bam"
+
+title="dist_coverage"
+std_pileup $if | \
+awk '{print $3}' | \
+sort -n | \
+uniq -c | \
+sort -k1,1rn | \
+tee ${title}.txt | \
+ruby -ane '\
+  BEGIN{puts "["} 
+  puts "{ x:#{$F[1]}, y:#{$F[0]} }," 
+  END{puts "]"}' \
+> ${title}.json
